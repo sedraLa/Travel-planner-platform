1893b5232cfb1890b8a5cc96b0fca96bb859e3ec
ayad2003 <ayadd2003@gamil.com>
2025-05-04 17:30:36 +0300

    aya edit

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -7,5 +7,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
+    //بعد التعديل
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'address',
+        'city',
+        'country',
+        'global_rating',
+        'price_per_night',
+        'total_rooms',
+        'destination_id',
+    ];
+
+    
+    protected $hidden = [
+        'created_at', 
+        'updated_at'
+    ];
+
+
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class, 'destination_id'); 
+    }
+
+    
+    public function images()
+    {
+        return $this->hasMany(HotelImage::class, 'hotel_id');
+    }
+
+
 }
