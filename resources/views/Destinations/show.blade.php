9c77cdfbef98f44b8ecc987613369a977d3368da
sedraLa <mhamadlaila42@gmail.com>
2025-05-07 12:04:50 -0700

    fix details page design and add details-layout

--- resources/views/Destinations/show.blade.php
+++ resources/views/Destinations/show.blade.php
@@ -1,14 +1,18 @@
-<x-app-layout>
+<x-details-layout>
     @push('styles')
         <link rel="stylesheet" href="{{asset('css/details.css')}}">
     @endpush
+    
     {{--body content--}}
 
 
     <div class="main-wrapper">
     <div class="hero-background" style="background-image: url('{{ $primaryImage ? asset('storage/' . $primaryImage->image_url) : '' }}');">
+        <div class="headings" style="font-size:32px;">
             <h1>{{$destination->name}}</h1>
             <h3>{{$destination->city}}</h3>
+        </div>
+           
         </div>
         </div>
 
@@ -16,7 +20,7 @@
             <header>Explore everything about this city</header>
             <div class="cards">
                 @foreach($destination->images as $image)
-                    @if(!$image->is_primary) 
+                    @if(!$image->is_primary)
                         <div class="card">
                             <img src="{{ asset('storage/' . $image->image_url) }}" alt="Destination Image">
                         </div>
@@ -33,7 +37,7 @@
             <p>{{$destination->description}}</p>
             </div>
             <div class="things">
-                <h1>Things to do in {{$destination->name}}</h1>
+                <h1>Things to do</h1>
                     <p>{{ $destination->activities }}</p>
             </div>
 
@@ -45,4 +49,4 @@
         </div>
     </div>
 
-    </x-app-layout>
+</x-details-layout>
