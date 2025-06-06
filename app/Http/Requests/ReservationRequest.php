4a2a9533b95d11a9310d0087f223b64600467286
sedraLa <mhamadlaila42@gmail.com>
2025-06-06 07:20:37 -0700

    Add ReservationRequest and success-error messages to reservation form

---
+++ app/Http/Requests/ReservationRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'guest_count' => 'required|integer|min:1',
+            'rooms_count' => 'required|integer|min:1',
+            'check_in_date' => 'required|date|after_or_equal:today',
+            'check_out_date' => 'required|date|after:check_in_date',
+            'name' => 'required|string',
+            'email' => 'required|email',
+        ];
+    }
+}
