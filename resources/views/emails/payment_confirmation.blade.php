bb0966351d0aefc99033977fdc9163a5a09c95c1
sedraLa <mhamadlaila42@gmail.com>
2025-06-08 08:48:23 -0700

    Implement payment confirmation email feature

---
+++ resources/views/emails/payment_confirmation.blade.php
@@ -0,0 +1,5 @@
+<h1>Payment Confirmation</h1>
+<p>Dear {{ $reservation->user->name }},</p>
+<p>Hotel Name: {{ $hotelName }}</p>
+<p>Your payment for this reservation has been confirmed.</p>
+<p>Thank you for your booking!</p>
