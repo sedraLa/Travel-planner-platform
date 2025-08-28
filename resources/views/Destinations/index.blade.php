0fca50dd5d8d68955b00f7a5b050f997c89d1a0d
sara <sarahgharizi@gmail.com>
2025-08-29 00:04:09 +0300

    favorite showed

--- resources/views/Destinations/index.blade.php
+++ resources/views/Destinations/index.blade.php
@@ -55,16 +55,27 @@
                         <h5>{{ $destination->name }}</h5>
                         <p class="overview">{{ Str::limit($destination->description, 80) }}</p>
                     </a>
-                    {{-- زر القلب --}}
+                    {{-- زر القلب المعدل مع أيقونات SVG أكبر --}}
                     <form action="{{ route('favorites.add', ['type' => 'destination', 'id' => $destination->id]) }}"
                         method="POST" class="fav-form">
                         @csrf
                         <button type="submit" class="fav-btn">
                             {{-- إذا كان عند المستخدم هالوجهة بالمفضلة --}}
                             @if(auth()->user()->favoriteDestinations->contains('id', $destination->id))
-                                ♥️
+                                {{-- قلب أحمر ممتلئ (كبير) --}}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-red-500" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                                        clip-rule="evenodd" />
+                                </svg>
                             @else
-                                ♡
+                                {{-- قلب فارغ (كبير) --}}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-500" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg>
                             @endif
                         </button>
                     </form>
