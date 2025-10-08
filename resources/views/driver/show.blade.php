698b844942c41aefb5513604b18802bdea12ab38
ayad2003 <ayadd2003@gamil.com>
2025-10-09 00:33:42 +0300

    driver_reservation1

--- resources/views/driver/show.blade.php
+++ resources/views/driver/show.blade.php
@@ -17,7 +17,7 @@
         {{--Reservation list --}}
         <div class="bg-white rounded-xl shadow-lg overflow-hidden">
             <div class="p-6 border-b border-gray-200">
-                <h2 class="text-2xl font-bold text-gray-800">Reservations List</h2>
+                <h2 class="text-2xl font-bold text-gray-800">Reservations {{ $driver->name }} </h2>
                 <p class="text-gray-500 mt-1">A list of all the reservations in your system for a driver.</p>
             </div>
 
@@ -25,9 +25,7 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                         <tr>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                ID</th>
+                            
                                 <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 vehicle</th>
@@ -62,7 +60,7 @@
                     <tbody class="bg-white divide-y divide-gray-200">
                 @forelse ($reservations as $reservation)
                     <tr>
-                        <td class="px-6 py-4 text-sm text-gray-900">{{ $reservation->id }}</td>
+                        <td class="px-6 py-4 text-sm text-gray-900"  class="hidden">{{ $reservation->id }}</td>
                         <td class="px-6 py-4 text-sm text-gray-900">{{ $reservation->vehicle->car_model ?? 'N/A'}}</td>
                         <td class="px-6 py-4 text-sm text-gray-900">{{ $reservation->pickup_location }}</td>
                         <td class="px-6 py-4 text-sm text-gray-900">{{ $reservation->dropoff_location }}</td>
