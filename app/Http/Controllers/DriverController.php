56de0cfe24653f3fcc5fb5b0fd7bf4fb86262f5f
ayad2003 <ayadd2003@gamil.com>
2025-12-10 03:30:42 +0300

    add colum to transport reservation with some edit

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -20,7 +20,7 @@ use Carbon\Carbon;
 class DriverController extends Controller
 {
     /**
-
+     
      */
 public function index(Request $request)
 {
@@ -33,7 +33,7 @@ public function index(Request $request)
         if (in_array(strtoupper($searchTerm), ['A', 'B'])) {
             $query->where('license_category', strtoupper($searchTerm));
         }
-
+        
          elseif (in_array($searchTerm, ['approved', 'pending', 'rejected'])) {
             $query->where('status', $searchTerm);
         }
@@ -55,7 +55,7 @@ public function index(Request $request)
 
 
     /**
-
+     
      */
     public function create()
     {
@@ -63,7 +63,7 @@ public function index(Request $request)
     }
 
     /**
-
+     
      */
     public function store(DriverRequest $request)
     {
@@ -71,11 +71,11 @@ public function index(Request $request)
         $user = User::create([
         'name' => $request->name,
         'email' => $request->email,
-        'password' => Hash::make($request->password),
+        'password' => Hash::make($request->password), 
         'last_name' => $request->last_name,
         'phone_number' => $request->phone_number,
         'country' => $request->country,
-        'role' => 'driver',
+        'role' => 'driver', 
     ]);
 
 
@@ -91,14 +91,14 @@ public function index(Request $request)
             'status'           => $request->status,
             'date_of_hire'     => $request->date_of_hire,
             'experience'       => $request->experience,
-
+          
         ]);
 
         return redirect()->route('drivers.index')->with('success', 'Driver created successfully');
     }
 
     /**
-
+     
      */
     public function show(string $id = null)
     {
@@ -118,7 +118,7 @@ public function index(Request $request)
 
 
         $reservations = $driver->reservations()
-        ->where('status', 'completed')
+        ->where('driver_status', 'completed')
         ->with(['vehicle', 'user'])
         ->get();
 
@@ -126,7 +126,7 @@ public function index(Request $request)
     }
 
     /**
-
+    
      */
 
 
@@ -137,7 +137,7 @@ public function index(Request $request)
 
     // جلب الحجوزات التي لا زالت Pending بدون أي تعديل تلقائي
     $reservations = $driver->reservations()
-        ->where('status', 'pending')
+        ->where('driver_status', 'pending')
         ->with(['vehicle', 'user'])
         ->get();
 
@@ -147,7 +147,7 @@ public function index(Request $request)
 
 
     /**
-
+    
      */
 
     public function edit(string $id)
@@ -157,7 +157,7 @@ public function index(Request $request)
     }
 
     /**
-
+     
      */
     public function update(DriverRequest $request, string $id)
 {
@@ -202,13 +202,13 @@ public function index(Request $request)
 
 
     /**
-
+   
      */
     public function destroy(string $id)
     {
         $driver = Driver::findOrFail($id);
 
-
+        
 
         if ($driver->license_image && Storage::disk('public')->exists($driver->license_image)) {
             Storage::disk('public')->delete($driver->license_image);
@@ -226,7 +226,7 @@ public function index(Request $request)
 
     public function updateStatus(DriverRequest $request, Driver $driver)
 {
-
+   
 
     if (auth()->user()->role !== 'admin') {
         abort(403, 'Unauthorized');
@@ -235,7 +235,7 @@ public function index(Request $request)
 
      $validated = $request->validated();
 
-
+   
    $updateData = ['status' => $validated['status']];
 
           if ($validated['status'] === 'approved') {
@@ -244,7 +244,7 @@ public function index(Request $request)
 
    $driver->update($updateData);
 
-
+  
     $status = $validated['status'];
     $message = match ($status) {
         'approved' => 'Your account has been approved! You can now log in to the system.',
@@ -265,9 +265,9 @@ public function index(Request $request)
 
         return redirect()->back()->with('success', 'Driver was rejected, email sent, and driver removed.');
     }
-
+    
     return redirect()->back()->with('success', 'Driver status updated and email sent successfully.');
-    }
+  } 
 
 
 
@@ -283,17 +283,9 @@ public function index(Request $request)
         return back()->with('error', 'Cannot complete this reservation before pickup time.');
     }
 
-    $reservation->update(['status' => 'completed']);
+    $reservation->update(['driver_status' => 'completed']);
 
     return redirect()->route('driverscompleted.show')->with('success', 'Reservation marked as completed.');
-}
-
-public function cancel($id)
-{
-    $reservation = TransportReservation::findOrFail($id);
-    $reservation->delete();
-
-    return redirect()->route('bookings.pending')->with('success', 'Reservation cancelled.');
 }
 
 
