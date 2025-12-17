00c4c7312ed37601eac022c3ef5c8764407f11c8
sedraLa <mhamadlaila42@gmail.com>
2025-12-17 10:34:27 -0800

    fix conflict

--- resources/views/driver/pendingbooking.blade.php
+++ resources/views/driver/pendingbooking.blade.php
@@ -35,7 +35,7 @@
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total Price</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                         
+
 
                         </tr>
                     </thead>
