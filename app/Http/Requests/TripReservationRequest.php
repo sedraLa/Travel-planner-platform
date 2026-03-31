cc4970d545aaa85b355f7f95effd435b29450f97
ayadd22 <engayad2003@gmail.com>
2026-04-01 01:32:34 +0300

    Add FormRequest classes for trip planning entities

---
+++ app/Http/Requests/TripReservationRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TripReservationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'day_activity_id' => 'required|exists:day_activities,id',
+            'guide_id' => 'required|exists:guides,id',
+            'status' => 'nullable|string|in:assigned,completed,cancelled',
+        ];
+    }
+}
